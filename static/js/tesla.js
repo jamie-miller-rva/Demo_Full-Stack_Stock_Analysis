9d2c7eff6391a4293befb67e4417605f8e8fc24e
Jamie-RVA <47825778+jamie-miller-rva@users.noreply.github.com>
2022-03-31 14:52:46 -0400

    create home page

--- static/js/tesla.js
+++ static/js/tesla.js
@@ -1,1 +1,7 @@
-const url =
+// access csv file saved in repository and available via url using github raw 
+const url = "https://raw.githubusercontent.com/jamie-miller-rva/Demo_Full-Stack_Stock_Analysis/main/static/data/all_stocks_5yr.csv";
+
+// Fetch the JSON data and console log it
+d3.csv(url).then(function(data) {
+    console.log(data);
+});
